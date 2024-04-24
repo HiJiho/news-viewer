5cfb9640a3200cbe26710680715f193a02905b55
김지호 <jiho001108@gmail.com>
2024-04-24 15:10:46 +0900

    환경변수에 뉴스 api 설정

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import axios from "axios";
 
 const App = () => {
 	const [data, setData] = useState(null);
-	const onClick = () => {
-		axios
-			.get("https://jsonplaceholder.typicode.com/todos/1")
-			.then((response) => {
-				setData(response.data);
-			});
+	const onClick = async () => {
+		try {
+			const response = await axios.get(
+				`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+			);
+			setData(response.data);
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	return (
@@ -16,13 +19,7 @@ const App = () => {
 			<div>
 				<button onClick={onClick}>불러오기</button>
 			</div>
-			{data && (
-				<textarea
-					rows={7}
-					value={JSON.stringify(data, null, 2)}
-					readOnly={true}
-				/>
-			)}
+			{data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
 		</div>
 	);
 };
