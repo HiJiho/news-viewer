d3fd796759bf5763b41f0768900da7360f9bbc28
김지호 <jiho001108@gmail.com>
2024-04-24 17:56:19 +0900

    spinner 추가

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import BounceLoader from "react-spinners/BounceLoader";
 
 const NewsListBlock = styled.div`
 	box-sizing: border-box; /* 패딩과 보더가 너비와 높이에 포함되도록 설정 */
@@ -9,6 +10,21 @@ const NewsListBlock = styled.div`
 	width: 768px; /* 고정 너비 */
 	margin: 0 auto; /* 좌우 자동 마진으로 중앙 정렬 */
 	margin-top: 2rem; /* 상단 마진 */
+	@media screen and (max-width: 768px) {
+		width: 100%; /* 화면 너비가 768px 이하일 때 너비 100%로 조정 */
+		padding-left: 1rem; /* 좌측 패딩 */
+		padding-right: 1rem; /* 우측 패딩 */
+	}
+`;
+
+const Spinner = styled.div`
+	box-sizing: border-box;
+	display: flex;
+	width: 768px;
+	margin: 0 auto;
+	margin-top: 2rem;
+	align-items: center;
+	justify-content: center;
 	@media screen and (max-width: 768px) {
 		width: 100%; /* 화면 너비가 768px 이하일 때 너비 100%로 조정 */
 		padding-left: 1rem; /* 좌측 패딩 */
@@ -39,7 +55,12 @@ const NewsList = () => {
 
 	// 대기 중일 때
 	if (loading) {
-		return <NewsListBlock>대기 중...</NewsListBlock>;
+		// return <NewsListBlock>대기 중...</NewsListBlock>;
+		return (
+			<Spinner>
+				<BounceLoader color="#000000" size={50} />
+			</Spinner>
+		);
 	}
 
 	// 아직 articles 값이 설정되지 않았을 때
